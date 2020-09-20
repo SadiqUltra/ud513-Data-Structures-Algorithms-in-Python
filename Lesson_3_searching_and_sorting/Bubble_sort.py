6f3b1efe24e7581d23e6a1d988ad4d38bb0dbd8e
sadiqultra <sadikultra@gmail.com>
2020-09-20 07:41:58 -0400

    Added: Bubble Sort

---
+++ Lesson_3_searching_and_sorting/Bubble_sort.py
@@ -0,0 +1,20 @@
+"""Implement quick sort in Python.
+Input a list.
+Output a sorted list."""
+
+
+def bubble_sort(array):
+    length = len(array)
+    previous = array[0]
+
+    for j in range(length):
+        for i in range(1, length):
+            if previous > array[i]:
+                array[i-1], array[i] = array[i], array[i-1]
+            previous = array[i]
+
+    return array
+
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print(bubble_sort(test))
